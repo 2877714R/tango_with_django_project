75eddfb2206feead4241ba668fb9dcd3aec355f7
Molly Robertson <2877714R@student.gla.ac.uk>
2025-02-06 19:07:02 +0000

    fixed error with urls.py

--- rango/urls.py
+++ rango/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
     path('about/', views.about, name='about'),
-    path('category/<slug:category_name_slug>/', views.show_category, name='show_category')
-    path('add_category/', views.add_category, name='add_category')
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+    path('add_category/', views.add_category, name='add_category'),
 ]
