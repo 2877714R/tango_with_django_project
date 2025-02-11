2ed43b740552a8f44e774df0d83d608814a912af
Molly Robertson <2877714R@student.gla.ac.uk>
2025-02-11 10:22:32 +0000

    finished chapter 8

--- rango/urls.py
+++ rango/urls.py
@@ -5,9 +5,6 @@ app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
     path('about/', views.about, name='about'),
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
     path('add_category/', views.add_category, name='add_category'),
